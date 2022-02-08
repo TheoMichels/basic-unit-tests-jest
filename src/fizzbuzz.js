74037743c1f8f56bc33bb77a1e43a2b47c88f25b
Théo Michels <theomichels@hotmail.fr>
2022-02-08 17:51:24 +0100

    implement fizzBuzz method & test

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,22 @@
 function fizzBuzz (n) {
-    // TODO: Compléter fizzbuzz()
-    return "fizzbuzz"
+
+    if(n == 0) {
+        return "Can't divise by 0"
+    }
+    else {
+        if(n%3 == 0 && n%5 != 0) {
+            return "Fizz"
+        }
+        if(n%5 == 0 && n%3 != 0) {
+            return "Buzz"
+        }
+        if(n%5 == 0 && n%3 == 0) {
+            return "FizzBuzz"
+        }
+        else {
+            return "False"
+        }
+    }
 }
 
 module.exports = fizzBuzz
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,21 @@
 const fizzBuzz = require("../src/fizzbuzz")
 
-test('TODO', () => {
-    // TODO: Tester fizzbuzz()
+test('fizzBuzz-divisible-by-5', () => {
+    expect(fizzBuzz(5)).toBe("Buzz")
+})
+
+test('fizzBuzz-divisible-by-3', () => {
+    expect(fizzBuzz(3)).toBe("Fizz")
+})
+
+test('fizzBuzz-divisible-by-3-and-5', () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz")
+})
+
+test('fizzBuzz-divisible-expect-to-be-false', () => {
+    expect(fizzBuzz(31)).toBe("False")
+})
+
+test('fizzBuzz-zero-is-divisible', () => {
+    expect(fizzBuzz(0)).toBe("Can't divise by 0")
 })
